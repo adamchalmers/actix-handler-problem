897cb80dda6beebeb0e2ce13ad84f4f21f4af0bf
Adam Chalmers <adam.s.chalmers@gmail.com>
2021-04-23 17:38:09 -0500

    Simplify example

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,19 @@
 use actix::prelude::*;
-use anyhow::Result;
 
 #[derive(Clone)]
 pub struct MyActor {
     pub input: Vec<u32>,
     pub output: Vec<u32>,
+}
+
+impl MyActor {
+    async fn shuffle(&mut self) -> Option<u32> {
+        let val = self.input.pop();
+        if let Some(val) = &val {
+            self.output.push(*val);
+        }
+        val
+    }
 }
 
 impl Actor for MyActor {
@@ -14,23 +23,15 @@ impl Actor for MyActor {
 /// When this is received, move a value from input to output,
 /// and return it.
 #[derive(Message)]
-#[rtype(result = "Result<u32>")]
+#[rtype(result = "Option<u32>")]
 pub struct MyMsg {}
 
 impl Handler<MyMsg> for MyActor {
-    type Result = ResponseActFuture<Self, Result<u32>>;
+    type Result = ResponseActFuture<Self, Option<u32>>;
 
     fn handle(&mut self, msg: MyMsg, ctx: &mut Context<Self>) -> Self::Result {
         let fut = async move { () }.into_actor(self).then(|_, actor, _ctx| {
-            let f = async move {
-                if let Some(val) = actor.input.pop() {
-                    actor.output.push(val.clone());
-                    Ok(val)
-                } else {
-                    Err(anyhow::anyhow!("No more input values!"))
-                }
-            }
-            .into_actor(actor);
+            let f = async move { actor.shuffle().await }.into_actor(actor);
             Box::pin(f)
         });
         Box::pin(fut)
